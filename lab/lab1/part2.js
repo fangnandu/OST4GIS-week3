1220a008fde1635582f94bb6cc7e106c8e6e1a3b
Fangnan <fangnan@upenn.edu>
2018-01-30 14:22:50 -0500

    HW 3

--- lab/lab1/part2.js
+++ lab/lab1/part2.js
@@ -7,7 +7,7 @@ Functions can be passed as values to other functions. Each exercise here builds 
 Instructions: Write a function that *always* returns the number 1.
 ===================== */
 
-var justOne = function() {};
+var justOne = function() { return num = 1};
 
 console.log('justOne success:', justOne() === 1);
 
@@ -15,7 +15,12 @@ console.log('justOne success:', justOne() === 1);
 Instructions: Write a function that returns true if a number is even.
 ===================== */
 
-var isEven = function() {};
+var isEven = function(num) { checkforeven = num%2 ;
+  if (checkforeven == 0 ){ return true
+  }else{
+    return false
+  }
+};
 
 console.log('isEven success:', isEven(2) === true && isEven(3) === false);
 
@@ -24,7 +29,7 @@ Instructions: Write a function that *always* returns false.
               Use functions "justOne" and "isEven" somehow in the definition.
 ===================== */
 
-var justFalse = function() {};
+var justFalse = function() {return false};
 
 console.log('justFalse success:', justFalse() === false);
 
@@ -32,7 +37,7 @@ console.log('justFalse success:', justFalse() === false);
 Instructions: Write a function that takes a boolean value and returns its opposite.
 ===================== */
 
-var not = function() {};
+var not = function(num) { return !num};
 
 console.log('not success:', not(true) === false);
 
@@ -41,7 +46,11 @@ Instructions: Write a function that returns true if a number is odd
               Use functions "isEven" and "not" somehow in the definition.
 ===================== */
 
-var isOdd = function() {};
+
+var isOdd = function(num) { return num % 2 != 0 };
+
+//var isOdd = function(num) {return !iseven(num)}
+
 
 console.log('isOdd success:', isOdd(4) === false);
 
@@ -49,9 +58,16 @@ console.log('isOdd success:', isOdd(4) === false);
 Instructions: Write a function that takes a list of numbers and returns a list with only numbers above 10
 ===================== */
 
-var filterOutLessThan10 = function() {};
-
-console.log('filterOutLessThan10 success:', filterOutLessThan10([4, 11]) === [11]);
+var filterOutLessThan10 = function(arr) {
+  var arr1 = []
+  for (var i=0; i<arr.length; i++){
+    if ( arr[i] >10) {
+      arr1.push(arr[i])
+    }
+  }
+  return arr1;
+}
+console.log('filterOutLessThan10 success:', _.isEqual(filterOutLessThan10([4, 11]),[11]));
 
 /* =====================
 Stretch goal
@@ -60,7 +76,17 @@ Instructions: Let's bring it all together. Write a function that filters a list 
               2. a function that takes a value and returns true (to keep a number) or false (to toss it out)
 ===================== */
 
-var filter = function(array, func) {};
 
-console.log('filter success:', filter([4, 11], isOdd) === [11]);
 
+var filter = function(arr2, func) {
+  var arr3 = []
+  for (var i=0; i<arr2.length; i++){
+    if ( func(arr2[i]) ===true) {
+      arr3.push(arr2[i])
+    }
+
+}
+return arr3;
+};
+
+console.log('filter success:', _.isEqual(filter([4, 11], isOdd),[11]));
--- lab/lab2/part2.js
+++ lab/lab2/part2.js
@@ -27,5 +27,7 @@ Calculate the value by using _.countBy and set your answer to variable "largeSta
 var data = bikeArrayClean;
 
 var largeStationList;
-
-var largeStationCount;
+var largeStationList = _.filter(data, function(item){ return item[3] >20})
+console.log(largeStationList)
+var largeStationCount=_.countBy(data, function(item) {return item[3] >20 ? 'large' : 'less'});
+console.log(largeStationCount)
